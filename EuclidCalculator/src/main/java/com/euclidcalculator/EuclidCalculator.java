4c3af633555bac4208746523c6a7bde37f6aa6ef
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-01-27 23:06:47 -0500

    Create GUI foundation

--- EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculator.java
+++ EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculator.java
@@ -12,7 +12,7 @@ public class EuclidCalculator extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EuclidCalculator.class.getResource("euclid-calculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Euclidean Algorithm Calculator");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
