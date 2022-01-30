6162c94fe061db9e3c22b34c611a9bc21ae91f8b
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-01-30 17:46:15 -0500

    Created GUI radio group

--- EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculator.java
+++ EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculator.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class EuclidCalculator extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EuclidCalculator.class.getResource("euclid-calculator.fxml"));
--- EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculatorController.java
+++ EuclidCalculator/src/main/java/com/euclidcalculator/EuclidCalculatorController.java
@@ -1,14 +1,33 @@
 package com.euclidcalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
-public class EuclidCalculatorController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EuclidCalculatorController implements Initializable {
 
     @FXML
-    protected void onConfirmSelection() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private RadioButton radioGCD;
+
+    @FXML
+    private RadioButton radioRSA;
+
+    private final ToggleGroup calculationOptions = new ToggleGroup();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.radioGCD.setToggleGroup(calculationOptions);
+        this.radioRSA.setToggleGroup(calculationOptions);
     }
+
+    @FXML
+    private void onConfirmSelection(ActionEvent event) {
+
+    }
+
 }
